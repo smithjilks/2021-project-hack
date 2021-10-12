fe4821c989d58979a87122b663a749bec868b11b
smithjilks <smithjilks@gmail.com>
2021-10-13 00:53:14 +0300

    Update register component

--- Software/stud-auth/front-end/stud-auth/src/app/auth/register/register.component.ts
+++ Software/stud-auth/front-end/stud-auth/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  private authStatusSub: Subscription;
 
-  ngOnInit(): void {
+  constructor(public authService: AuthService){}
+
+  ngOnInit(): void{
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus =>{
+        this.isLoading = false
+      }
+    );
+  }
+
+
+  onSignup(form: NgForm): void{
+    if(form.invalid){
+      return
+    }
+    this.isLoading = true;
+
+    this.authService.createUser(form.value.email, form.value.password);
+
+  }
+
+  ngOnDestroy(): void{
+    this.authStatusSub.unsubscribe();
+
   }
 
 }
