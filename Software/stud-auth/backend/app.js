8a008cc204722e7a0339207c7dc365d7ef826906
smithjilks <smithjilks@gmail.com>
2021-10-13 00:59:49 +0300

    Update app.js

--- Software/stud-auth/backend/app.js
+++ Software/stud-auth/backend/app.js
@@ -11,34 +11,26 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 
-mongoose.connect("mongodb+srv://smith:" + process.env.MONGO_ATLAS_PW + "@cluster0.ptmmu.mongodb.net/node-angular?retryWrites=true&w=majority", {useNewUrlParser: true,  useUnifiedTopology: true } )
+mongoose.connect("mongodb+srv://smith:" + process.env.MONGO_ATLAS_PW + "@cluster0.ptmmu.mongodb.net/stud-auth?retryWrites=true&w=majority", {useNewUrlParser: true,  useUnifiedTopology: true } )
 .then( () =>{
   console.log("Connected to Database");
 })
 .catch(()=>{
   console.log('Connection failed');
 });
-
-//app.use(cors());
+ 
+//body-parser is depracated. Consider changing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//granting access to the images folder
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-//set headers to disablle CORS error that is triggered bb default
-//CORS - Cross Origin Resource Error
-
 app.use((req, res, next) => {
-
-  //Allows domains to access resources
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS, PUT");
-
-  //Restrict to domains sending request with a set of particular headers
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
