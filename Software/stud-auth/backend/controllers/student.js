93d48ffa7012c2624554e3147002054e25b2c57b
smithjilks <smithjilks@gmail.com>
2021-10-13 00:54:31 +0300

    Add controllers

---
+++ Software/stud-auth/backend/controllers/student.js
@@ -0,0 +1,131 @@
+const Student = require('../models/student');
+
+
+exports.createStudent = (req, res) => {
+  const url = req.protocol + "://" + req.get("host");
+  const student = new Student({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        regNumber: req.body.regNumber,
+        imagePath: url + "/images/" + req.file.filename,
+        rfidTag: req.body.rfidTag
+  });
+
+  student.save().then(createdStudent =>{
+    res.status(201).json({
+      mesaage: "Student added successfully",
+      sudent: {
+        ...createdStudent,
+        id: createdStudent._id
+      }
+    });
+  }).catch(error =>{
+    res.status(500).json({
+      message: "creating a student failed!"
+    })
+  });
+}
+
+exports.updateStudent = (req, res, next) =>{
+  let imagePath = req.body.imagePath;
+  if(req.file){
+    const url = req.protocol + "://" + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+
+  }
+  const student = new Student({
+    _id: req.body.id,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    regNumber: req.body.regNumber,
+    magePath: imagePath,
+    rfidTag: req.body.rfidTag
+  });
+
+  Student.updateOne({_id: req.params.id}, student).then(result => {
+    if(result.n > 0){
+      res.status(200).json({ message: 'Update successful'})
+    }else{
+      res.status(401).json({ message: 'Not Authorized'})
+    }
+
+  }).catch(error =>{
+    res.status(500).json({
+      message : "Couldn't update student!"
+    });
+  });
+}
+
+exports.getStudents = (req, res, next) => {
+
+  const pageSize = +req.query.pagesize;  
+  const currentPage = +req.query.page;
+  const studentQuery = Student.find();
+  let fetchedStudents;
+
+  if(pageSize && currentPage){
+    studentQuery
+    .skip( pageSize * (currentPage - 1))
+    .limit(pageSize);
+  }
+
+
+  studentQuery.then( documents => {
+    fetchedStudent = documents;
+    return Student.countDocuments();
+  })
+  .then(count =>{
+    res.status(200).json({
+      message: 'Succesfully sent from api',
+      body: fetchedStudents,
+      maxStudents: count
+    });
+  })
+  .catch(error =>{
+    res.status(500).json({
+      message: "Fetching students failed!"
+    });
+  });
+
+
+
+}
+
+exports.getStudent = (req, res, next) =>{
+
+  Student.findById(req.params.id).then(student =>{
+    if(student){
+      res.status(200).json(student);
+    }
+    else{
+      res.status(404).json({
+        message: "student not found"
+      })
+    }
+  })
+  .catch(error =>{
+    res.status(500).json({
+      message: "Fetching student failed!"
+    });
+  });
+}
+
+exports.deleteStudent = (req, res, next) => {
+
+  Student.deleteOne({ _id: req.params.id}).then(result =>{
+
+    if(result.n > 0){
+      res.status(200).json({ message: 'Deletion successful'})
+    }else{
+      res.status(401).json({ message: 'Not Authorized'})
+    }
+  })
+  .catch(error =>{
+    res.status(500).json({
+      message: "Deleting student failed!"
+    });
+  });
+
+}
---
+++ Software/stud-auth/backend/controllers/tag.js
@@ -0,0 +1,161 @@
+const Tag = require('../models/tag');
+const Student = require('../models/student');
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  //encrypted: process.env.PUSHER_APP_SECURE,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  useTLS: process.env.USETLS,
+});
+
+exports.validateTag = (req, res) => {
+        rfidTag = req.body.rfid;
+
+        Tag.findOne({rfid: rfidTag}).then(document => {
+                if(document.n > 0 && document.isAssigned == true && document.isActive == true){
+                        
+                        //send pusher to angular app
+                        Student.findOne({rfidTag: rfidTag}).then(student => {
+                          if(student.n > 0){
+                            pusher.trigger("display-channel", "stud-auth", {
+                              message: "Student found",
+                              student: student
+                            });
+                          }
+                          res.status(200).json({ message: 'Tag holder found successfully'});
+
+                        }).catch(error => {
+                          res.status(500).json({
+                            message : "Couldn't retrieve student!"
+                          });
+                        })
+                      
+                }else if(document.n > 0 && document.isAssigned == false && document.isActive == true){
+                        //send pusher to angular app for adding dstudent
+
+                        pusher.trigger("entry-channel", "stud-entry", {
+                          message: "Tag not assigned",
+                          rfid: rfid
+                        });
+
+                        res.status(200).json({ message: 'Tag not assigned'});
+
+                } else if(document.n > 0 && document.isAssigned == true && document.isActive == false) {
+                        res.status(401).json({ message: 'Not Authorized. No longer active'});
+                
+                } else {
+                        const tag = new Tag({
+                                rfid: rfidTag,
+                                isAssigned: false,
+                                isActive: true
+                        });
+                        tag.save().then(createdTag =>{
+                            res.status(201).json({
+                              mesaage: "Tag added successfully",
+                              tag: {
+                                ...createdTag,
+                                id: createdTag._id
+                              }
+                            });
+                          }).catch(error =>{
+                            res.status(500).json({
+                              message: "creating a tag failed!"
+                            })
+                          });
+
+                }
+        });
+  
+}
+
+exports.updateTag = (req, res, next) =>{
+  const tag = new Tag({
+    _id: req.body.id,
+    rfid: req.body.rfid,
+    isAssigned: req.body.isAssigned,
+    isActive: req.body.isActive,
+  });
+
+  Tag.updateOne({_id: req.params.id}, tag).then(result => {
+    if(result.n > 0){
+      res.status(200).json({ message: 'Update successful'})
+    }else{
+      res.status(401).json({ message: 'Not Authorized'})
+    }
+
+  }).catch(error =>{
+    res.status(500).json({
+      message : "Couldn't update tag!"
+    });
+  });
+}
+
+exports.getTags = (req, res, next) => {
+  const pageSize = +req.query.pagesize;  
+  const currentPage = +req.query.page;
+  const tagQuery = Tag.find();
+  let fetchedTags;
+
+  if(pageSize && currentPage){
+    fetchedTags
+    .skip( pageSize * (currentPage - 1))
+    .limit(pageSize);
+  }
+
+
+  tagQuery.then( documents => {
+        fetchedTags = documents;
+        return Tag.countDocuments();
+  })
+  .then(count =>{
+    res.status(200).json({
+      message: 'Succesfully sent from api',
+      body: fetchedTags,
+      maxTags: count
+    });
+  })
+  .catch(error =>{
+    res.status(500).json({
+      message: "Fetching tags failed!"
+    });
+  });
+}
+
+
+exports.getTag = (req, res, next) =>{
+  Tag.findById(req.params.id).then(tag =>{
+    if(tag){
+      res.status(200).json(tag);
+    }
+    else{
+      res.status(404).json({
+        message: "tag not found"
+      })
+    }
+  })
+  .catch(error => { 
+    res.status(500).json({
+      message: "Fetching tag failed!"
+    });
+  });
+}
+
+exports.deleteTag = (req, res, next) => {
+
+  Tag.deleteOne({ _id: req.params.id}).then(result =>{
+
+    if(result.n > 0){
+      res.status(200).json({ message: 'Deletion successful'})
+    }else{
+      res.status(401).json({ message: 'Not Authorized'})
+    }
+  })
+  .catch(error =>{
+    res.status(500).json({
+      message: "Deleting tag failed!"
+    });
+  });
+}
