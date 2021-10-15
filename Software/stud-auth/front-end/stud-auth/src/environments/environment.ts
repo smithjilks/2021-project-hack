3f1a45533a48f617d04c2191bf7bdb445bcec632
smithjilks <smithjilks@gmail.com>
2021-10-15 20:29:03 +0300

    Update environment config

--- Software/stud-auth/front-end/stud-auth/src/environments/environment.ts
+++ Software/stud-auth/front-end/stud-auth/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/v1/api/"
-
+  apiUrl: "http://localhost:3000/v1/api",
+  pusherAppKey: "e26a0a7635661d971f02",
+  pushAppCluster: "ap2",
 };
